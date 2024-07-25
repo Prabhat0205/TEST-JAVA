b23712193514d2faa79d2194224a2638196cd807
Prabhat0205 <prabhat.appzoro@gmail.com>
2024-07-25 06:06:35 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+  public static void main(String[] args) {
+    String myString = "My name ";
+    String myname  = "is Prabhat ";
+    String lastname = "Garg"; //58598980kipo7k74h5jkl78jk7  \"it\"'s";
+  
+    String fullname = myString + myname + lastname;
+    System.out.println(fullname);
+  }
+}
